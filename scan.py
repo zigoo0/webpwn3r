9fa54acb3e8fdf277075590e4b83520981463382
lnxg33k <ahmedelantry@gmail.com>
2014-05-17 17:40:29 +0300

    Keep asking if invalid url supplied

--- scan.py
+++ scan.py
@@ -32,18 +32,17 @@ print ga.green+'''
 def urls_or_list():
 	url_or_list = raw_input(" [!] Scan URL or List of URLs? [1/2]: ")
 	if url_or_list == "1":
-	 	 url = raw_input(" [!] Enter the URL: ")
-		 if not url.startswith("http://"):
+	 	 url = raw_input(" [!] enter the url (e.g. http://localhost/index.php?id=1): ")
+		 while (not ("?" and "=") in url) or not url.startswith("http://"):
 		     #Thanks to Nu11 for the HTTP checker
-                     print ga.red+'''\n Invalid URL, Please Make Sure That The URL Starts With \"http://\" \n'''+ga.end
-                     exit()
+		     print ga.red +"\n [Warning] "+ ga.end + ga.bold+"%s"%url +ga.end + ga.red +" is not a valid URL"+ga.end			
+		     print ga.red +" [Warning] You should write a Full URL .e.g http://site.com/page.php?id=value \n"+ ga.end
+                     #print ga.red+'''\n Invalid URL, Please Make Sure That The URL Starts With \"http://\" \n'''+ga.end
+	 	     url = raw_input(" [!] enter the url: ")
+                     #exit()
 		 if "?" in url:
 		 	rce_func(url)
 		 	xss_func(url)		 
-		 else:
-			print ga.red +"\n [Warning] "+ ga.end + ga.bold+"%s"%url +ga.end + ga.red +" is not a valid URL"+ga.end			
-			print ga.red +" [Warning] You should write a Full URL .e.g http://site.com/page.php?id=value \n"+ ga.end
-			exit()
 	if url_or_list =="2":
 		 urls_list = raw_input( ga.green+" [!] Enter the list file name .e.g [list.txt]: "+ga.end)
 		 open_list = open(urls_list).readlines()
