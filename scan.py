704d34793fda5f79fc702c48d2d7f9b126742274
zigoo0 <zigoo0@users.noreply.github.com>
2014-05-17 16:49:52 +0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -21,22 +21,45 @@ print ga.green+'''
         #|  By Ebrahim Hegazy - @Zigoo0                              #
         #|  This Version Supports Remote Code/Command Execution, XSS #
 	#|  Thanks @lnxg33k, @dia2diab @Aelhemily, @okamalo          #
+	#|  More Details: http://www.sec-down.com/wordpress/?p=373   #
         ##############################################################
         '''+ga.end
 
 def urls_or_list():
-	url_or_list = raw_input(" [!] Scan URL or List? [1/2]: ")
+	url_or_list = raw_input(" [!] Scan URL or List of URLs? [1/2]: ")
 	if url_or_list == "1":
-	 	 url = raw_input( ga.green+" [!] Enter the URL: "+ga.end)
-		 rce_func(url)
-		 xss_func(url)
+	 	 url = raw_input(" [!] Enter the URL: ")
+		 if not url.startswith("http://"):
+		     #Thanks to Nu11 for the HTTP checker.
+                     print ga.red+'''\n Invalid URL, Please Make Sure That The URL Starts With \"http://\" \n'''+ga.end
+                     exit()
+		 if "?" in url:
+		 	rce_func(url)
+		 	xss_func(url)		 
+		 else:
+			print ga.red +"\n [Warning] "+ ga.end + ga.bold+"%s"%url +ga.end + ga.red +" is not a valid URL"+ga.end			
+			print ga.red +" [Warning] You should write a Full URL .e.g http://site.com/page.php?id=value \n"+ ga.end
+			exit()
 	if url_or_list =="2":
 		 urls_list = raw_input( ga.green+" [!] Enter the list file name .e.g [list.txt]: "+ga.end)
 		 open_list = open(urls_list).readlines()
 		 for line in open_list:
-			 links = line.strip()
-		  	 url = links
-		  	 print ga.green+" \n [!] Now Scanning %s"%url +ga.end
-		  	 rce_func(url)
-			 xss_func(url)
+			 if "?" in line:
+			 	links = line.strip()
+		  	 	url = links
+		  	 	print ga.green+" \n [!] Now Scanning %s"%url +ga.end
+		  	 	rce_func(url)
+			 	xss_func(url)
+			 else:
+			 	links = line.strip()
+		  	 	url = links
+				print ga.red +"\n [Warning] "+ ga.end + ga.bold+"%s"%url +ga.end + ga.red +" is not a valid URL"+ga.end				
+				print ga.red +" [Warning] You should write a Full URL .e.g http://site.com/page.php?id=value \n"+ ga.end
+		 exit()				
+
 urls_or_list()
+
+
+
+
+
