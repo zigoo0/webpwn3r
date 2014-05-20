2593107e4be9ace479c1fe6c11173ff41d59ec9a
zigoo0 <zigoo0@users.noreply.github.com>
2014-05-21 01:29:37 +0300

    Update vulnz.py
    Editing Code Comments.

--- vulnz.py
+++ vulnz.py
@@ -74,11 +74,8 @@ def error_based_sqli_func(url):
 	print ga.bold+"\n [!] Now Scanning for Error Based SQL Injection "+ga.end
 	print ga.blue+" [!] Covering MySQL, Oracle, MSSQL, MSACCESS & PostGreSQL Databases "+ga.end
 	print ga.blue+" [!] Please wait ...."+ga.end
-	# Payload = 12345'"\'\");|]*{%0d%0a<%00>%bf%27'
-	# add chinese char to the SQLI payloads to bypass mysql_escape_char
+	# Payload = 12345'"\'\");|]*{%0d%0a<%00>%bf%27'  Yeaa let's bug the query :D :D
+	# added chinese char to the SQLI payloads to bypass mysql_real_escape_*
 	payloads = ["3'", "3%5c", "3%27%22%28%29", "3'><", "3%22%5C%27%5C%22%29%3B%7C%5D%2A%7B%250d%250a%3C%2500%3E%25bf%2527%27"]
-	# Still waiting for ORACLE, MYSQL Signatures to be added.
-	# MYSQL = SQL syntax
-	# MSSQL - Input string
 	check = re.compile("Incorrect syntax|Syntax error|Unclosed.+mark|unterminated.+qoute|SQL.+Server|Microsoft.+Database|Fatal.+error", re.I)
 	main_function(url, payloads, check)
